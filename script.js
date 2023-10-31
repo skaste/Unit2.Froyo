abaa5d935b6ccef09c0089d11a9453cd6e589abe
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-10-31 14:41:10 -0500

    updated the pseudocode to figure out the pathway for required logging

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // console.log(`Test for the FroYo!!!`);
 //create prompt to collect user flavors
 const userInputFlavors = prompt(`Please enter Froyo flavors with commas`, 
-"strawberry, vanilla, chocolate, black cherry, etc");
+"strawberry, vanilla, chocolate, black cherry");
 //Take user input strings and split into an array of strings using const array = stringList.split(", ")
 const stringArray = userInputFlavors.split(", ");
-console.log(stringArray);
+// console.log(stringArray);
+//create an object with keys that are flavors
+//have stringArray push userInputFlavors into the object as keys
+//get the sum of the keys for the value
