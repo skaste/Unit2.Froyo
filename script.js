9bd4b1644683fef3566842258514b9327449f041
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-10-31 14:30:47 -0500

    created a string array from user input flavors and validated it worked by logging to console

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(`Test for the FroYo!!!`);
+// console.log(`Test for the FroYo!!!`);
+//create prompt to collect user flavors
+const userInputFlavors = prompt(`Please enter Froyo flavors with commas`, 
+"strawberry, vanilla, chocolate, black cherry, etc");
+//Take user input strings and split into an array of strings using const array = stringList.split(", ")
+const stringArray = userInputFlavors.split(", ");
+console.log(stringArray);
